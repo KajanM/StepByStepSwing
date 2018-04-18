4caf719560aaf3450c39e2df6711c42fa6ca669c
K4J4N <kajan.14@cse.mrt.ac.lk>
2018-04-18 20:22:36 +0530

    decoupled text panel and tool bar using a custom listener

---
+++ Swing7- Listeners/src/MainFrame.java
@@ -0,0 +1,37 @@
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class MainFrame extends JFrame {
+	
+	private final Toolbar toolbar;
+	private final TextPanel textPanel;
+	
+	public MainFrame() {
+		super("Hello World");
+		
+		toolbar = new Toolbar();
+		textPanel = new TextPanel();
+		
+		toolbar.setToolbarListener(new ToolbarListener() {
+			
+			@Override
+			public void actionPerformed(String text) {
+				textPanel.appendText(text);
+			}
+		});
+		
+		setLayout(new BorderLayout());
+		
+		add(toolbar, BorderLayout.NORTH);
+		add(textPanel, BorderLayout.CENTER);
+		
+		setSize(600, 500);
+		setVisible(true);
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+	}
+}
---
+++ Swing7- Listeners/src/Toolbar.java
@@ -0,0 +1,54 @@
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Toolbar extends JPanel implements ActionListener {
+	private static final Logger log = LogManager.getLogger(Toolbar.class);
+	
+	private final JButton helloBtn;
+	private final JButton goodByeBtn;
+	
+	private ToolbarListener toolbarListener;
+		
+	public Toolbar() {
+		helloBtn = new JButton("Hello");
+		goodByeBtn = new JButton("GoodBye");
+		
+		helloBtn.addActionListener(this);
+		goodByeBtn.addActionListener(this);
+		
+		setLayout(new FlowLayout(FlowLayout.LEFT));
+		
+		add(helloBtn);
+		add(goodByeBtn);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String msg = null;
+		JButton clicked = (JButton)e.getSource();
+		
+		if(clicked == helloBtn) {
+			log.debug("helloBtn clicked");
+			msg = "Hello\n";
+		} else if(clicked == goodByeBtn) {
+			log.debug("goodByeBtn clicked");
+			msg = "GoodBye\n";
+		}
+		
+		if(toolbarListener != null) {
+			toolbarListener.actionPerformed(msg);
+		}
+	}
+
+	public void setToolbarListener(ToolbarListener toolbarListener) {
+		this.toolbarListener = toolbarListener;
+	}
+
+}
