6726c44c7f21c2b96d38385aef3401b77902610c
K4J4N <kajan.14@cse.mrt.ac.lk>
2018-04-19 12:13:43 +0530

    imported from previous project

---
+++ Swing18- Menus/src/FormEvent.java
@@ -0,0 +1,57 @@
+import java.util.EventObject;
+
+public class FormEvent extends EventObject {
+	private static final long serialVersionUID = -6293008922586000503L;
+
+	private String name;
+	private String occupation;
+	private int ageCatId;
+	private String empCategory;
+	private boolean isSLCitizen;
+	private String taxID;
+	private String gender;
+
+	public FormEvent(Object source) {
+		super(source);
+	}
+
+	public FormEvent(Object source, String name, String occupation, int id, String empCategory, boolean isSLCitizen,
+			String taxID, String gender) {
+		super(source);
+		this.name = name;
+		this.occupation = occupation;
+		this.ageCatId = id;
+		this.empCategory = empCategory;
+		this.isSLCitizen = isSLCitizen;
+		this.taxID = taxID;
+		this.gender = gender;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getOccupation() {
+		return occupation;
+	}
+
+	public int getAgeCatId() {
+		return ageCatId;
+	}
+
+	public String getEmpCategory() {
+		return empCategory;
+	}
+
+	public boolean isSLCitizen() {
+		return isSLCitizen;
+	}
+
+	public String getTaxID() {
+		return taxID;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+}
